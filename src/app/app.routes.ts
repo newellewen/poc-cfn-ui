3da2f7c6442968a1b5127016c9a5ef39158c430d
newellewen <newell.philip.86@gmail.com>
2025-06-01 18:03:16 -0400

    Set up sandbox component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { WalletComponent } from './pages/wallet/wallet.component';
+import { SandboxComponent } from './pages/sandbox/sandbox.component';
 
 export const routes: Routes = [
-    { path: 'wallet', component: WalletComponent }
+    { path: 'wallet', component: WalletComponent },
+    { path: 'sandbox', component: SandboxComponent }
   ];
---
+++ src/app/pages/sandbox/sandbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sandbox',
+  imports: [],
+  templateUrl: './sandbox.component.html',
+  styleUrl: './sandbox.component.scss'
+})
+export class SandboxComponent {
+
+}
